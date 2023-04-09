a28ba383069daf146105e42be29739b1eb308bdd
KaBleeth <kableeth@gmail.com>
2023-04-08 23:13:40 -0500

    updated port

--- app.py
+++ app.py
@@ -26,5 +26,4 @@ def process_text():
     return jsonify({'result': result})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
